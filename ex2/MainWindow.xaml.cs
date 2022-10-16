4fb3b7d56e07dc91cdbc7bea8a8adbeca9b8ad9e
Maxim Zolotarev <maxamar2113@gmail.com>
2022-10-16 14:00:07 +0300

    Vompleted

--- ex2/MainWindow.xaml.cs
+++ ex2/MainWindow.xaml.cs
@@ -23,43 +23,90 @@ namespace ex2
         public const double SMALL_PRICE = 5.00;
         public const  double MEDIUM_PRICE = 6.25;
         public const double LARGE_PRICE = 7.75;
+        public const double TOPPING_PRICE = 0.5;
+        public double currentNumber = 0;
         public MainWindow()
         {
             InitializeComponent();
-
-            
         }
 
 
 
         private void smallRadioButton_Checked(object sender, RoutedEventArgs e)
-        {   
-            priceLabel.Content = $"Price is: {SMALL_PRICE}$";
+        {
+            currentNumber += SMALL_PRICE;
+            contentUpdate();
         }
         private void mediumRadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            priceLabel.Content = MEDIUM_PRICE;
+            currentNumber += MEDIUM_PRICE;
+            contentUpdate();
         }
 
         private void largeRadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            priceLabel.Content = LARGE_PRICE;
+            currentNumber += LARGE_PRICE;
+            contentUpdate();
+        }
+        private void smallRadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            currentNumber -= SMALL_PRICE;
+        }
+
+        private void mediumRadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            currentNumber -= MEDIUM_PRICE;
+        }
+
+        private void largeRadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            currentNumber -= LARGE_PRICE;
         }
 
         private void onionCheckBox_Checked(object sender, RoutedEventArgs e)
         {
-
+            toppingAdded();
         }
 
         private void pepperCheckBox_Checked(object sender, RoutedEventArgs e)
         {
-
+            toppingAdded();
         }
 
         private void tomatoCheckBox_Checked(object sender, RoutedEventArgs e)
         {
-
+            toppingAdded();
         }
 
+        private void onionCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            toppingRemoved();
+        }
+
+        private void pepperCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            toppingRemoved();
+        }
+
+        private void tomatoCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            toppingRemoved();
+        }
+        private void contentUpdate()
+        {
+            priceLabel.Content = $"Price is: {currentNumber}";
+        }
+
+        private void toppingAdded()
+        {
+            currentNumber += TOPPING_PRICE;
+            contentUpdate();
+        }
+
+        private void toppingRemoved()
+        {
+            currentNumber -= TOPPING_PRICE;
+            contentUpdate();
+        }
     }
 }
